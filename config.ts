1467277e799d0d5556b5e9e77571e22079a43280
itzmerai <159508958+itzmerai@users.noreply.github.com>
2025-04-14 01:18:47 +0800

    for deployment and build

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ interface Config {
 }
 
 const Config: Config = {
-  API_BASE_URL: "http://192.168.1.7:5000", // Replace with your IP address
+  API_BASE_URL: "https://revisionstructuredbackend.vercel.app", // Replace with your IP address
 };
 
 export default Config;
